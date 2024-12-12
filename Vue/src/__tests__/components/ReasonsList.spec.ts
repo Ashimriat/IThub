772df55171f2f150b7f2eb09afb3fffc3640cbc0
Ashimriat <ashimriat42@gmail.com>
2024-12-12 23:17:04 +0300

    Поправил тест

--- Vue/src/__tests__/components/ReasonsList.spec.ts
+++ Vue/src/__tests__/components/ReasonsList.spec.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import { nextTick } from 'vue';
 import { mount } from '@vue/test-utils';
 import { REASONS_LIST } from '@/components/ContentWindow/constants';
 import ReasonsList from '@/components/ContentWindow/elements/Reasons.vue';
@@ -7,12 +8,12 @@ import ReasonsList from '@/components/ContentWindow/elements/Reasons.vue';
 describe('ProgressBar', () => {
   it.each([
     0, 3, 5
-  ])('Корректно рендерится c разным количеством причин: [%d]', (lastIndex) => {
-    const wrapper = mount(ReasonsList, { 
-      props: {
-        reasons: REASONS_LIST.slice(0, lastIndex),
-      }
-    });
+  ])('Корректно рендерится c разным количеством причин: [%d]', async (lastIndex) => {
+    const wrapper = mount(ReasonsList);
+    for (let i = 0; i < lastIndex; i++) {
+      wrapper.vm.addListItem();
+    }
+    await nextTick();
     const li = wrapper.findAll('li');
     expect(li.length).toEqual(lastIndex);
     for (let i = 0; i < lastIndex; i++) {
